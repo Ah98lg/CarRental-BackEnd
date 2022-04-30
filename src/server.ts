ec2cab1ee889413caca74db2e0c3f39f210751d5
Arthur <ah98lg@gmail.com>
2022-04-29 23:03:01 -0300

    Installation of new libs and yours @types

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,24 @@
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
+import { Request, Response } from 'express';
 
-const app = express ();
+const app = express();
 
-app.listen(3333, ()=> console.log("Server is running"));
+app.listen(3333);
+
+const categories = []
+
+app.post("/" , (request : Request, response : Response) => {
+    const {name, type} = request.body;
+
+    const category = {
+        name,
+        id: uuidv4(),
+        type,
+        created_at: new Date()
+    }
+
+    categories.push(category)
+
+    return response.status(201).json(category)
+})
