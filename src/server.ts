5bb3baa032de6fede571c69b8fbc342d8652c7d4
Arthur <ah98lg@gmail.com>
2022-04-29 23:07:11 -0300

    Creation of the first route file

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,8 @@
 import express from 'express'
-import { v4 as uuidv4 } from 'uuid'
-import { Request, Response } from 'express';
+import { CreateCategory } from './routes/CreateCategory.routes';
 
 const app = express();
 
 app.listen(3333);
 
-const categories = []
-
-app.post("/" , (request : Request, response : Response) => {
-    const {name, type} = request.body;
-
-    const category = {
-        name,
-        id: uuidv4(),
-        type,
-        created_at: new Date()
-    }
-
-    categories.push(category)
-
-    return response.status(201).json(category)
-})
+app.post("/categories" , CreateCategory)
